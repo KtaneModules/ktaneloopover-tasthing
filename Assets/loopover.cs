e1e35f86f6cc57994671a7a5633b7831eba68c32
Timwi <github@timwi.de>
2019-11-01 03:53:44 +0100

    Merge the button handlers into one; add tick.wav

--- Assets/loopover.cs
+++ Assets/loopover.cs
@@ -1,5 +1,5 @@
 using System;
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,13 +7,13 @@ using KModkit;
 using rnd = UnityEngine.Random;
 
 public class loopover : MonoBehaviour
- {
-	 	public KMAudio Audio;
-		public KMBombInfo bomb;
+{
+    public KMAudio Audio;
+    public KMBombInfo bomb;
 
-		static int moduleIdCounter = 1;
-		int moduleId;
-		private bool moduleSolved;
+    static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
 
     public Renderer[] tiles;
     public Material[] tileColors;
@@ -26,159 +26,141 @@ public class loopover : MonoBehaviour
     public KMSelectable[] poshoributtons;
     public KMSelectable[] neghoributtons;
 
-
-		void Awake()
-		{
-        moduleId = moduleIdCounter++;
-        foreach (KMSelectable arrow in negvertibuttons)
+    KMSelectable.OnInteractHandler buttonHandler(KMSelectable arrow, Action<KMSelectable> method)
+    {
+        return delegate ()
         {
-          arrow.OnInteract += delegate ()
-          {
-            if(!moduleSolved)
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+            arrow.AddInteractionPunch();
+            if (!moduleSolved)
             {
-              negvertiPress(arrow);
-              tileState();
-            }
-            return false;
-          };
-        }
-        foreach (KMSelectable arrow in posvertibuttons)
-        {
-          arrow.OnInteract += delegate ()
-          {
-            if(!moduleSolved)
-            {
-              posvertiPress(arrow);
-              tileState();
-            }
-            return false;
-          };
-        }
-        foreach (KMSelectable arrow in poshoributtons)
-        {
-          arrow.OnInteract += delegate ()
-          {
-            if(!moduleSolved)
-            {
-                poshoriPress(arrow);
+                method(arrow);
                 tileState();
             }
             return false;
-          };
+        };
+    }
+
+    void Awake()
+    {
+        moduleId = moduleIdCounter++;
+        foreach (KMSelectable arrow in negvertibuttons)
+        {
+            arrow.OnInteract += buttonHandler(arrow, negvertiPress);
+        }
+        foreach (KMSelectable arrow in posvertibuttons)
+        {
+            arrow.OnInteract += buttonHandler(arrow, posvertiPress);
+        }
+        foreach (KMSelectable arrow in poshoributtons)
+        {
+            arrow.OnInteract += buttonHandler(arrow, poshoriPress);
         }
         foreach (KMSelectable arrow in neghoributtons)
         {
-          arrow.OnInteract += delegate ()
-          {
-            if(!moduleSolved)
-            {
-              neghoriPress(arrow);
-              tileState();
-            }
-            return false;
-          };
+            arrow.OnInteract += buttonHandler(arrow, neghoriPress);
         }
-		}
+    }
 
-		void Start ()
-		{
-      currentState = solveState.ToArray();
-      for(int i = 0; i < 1000; i++)
-      {
-        switch (rnd.Range(0,4))
+    void Start()
+    {
+        currentState = solveState.ToArray();
+        for (int i = 0; i < 1000; i++)
         {
-          case 0:
-            negvertiPress(negvertibuttons[rnd.Range(0,5)]);
-            break;
-          case 1:
-            posvertiPress(posvertibuttons[rnd.Range(0,5)]);
-            break;
-          case 2:
-            neghoriPress(neghoributtons[rnd.Range(0,5)]);
-            break;
-          default:
-            poshoriPress(poshoributtons[rnd.Range(0,5)]);
-            break;
+            switch (rnd.Range(0, 4))
+            {
+                case 0:
+                    negvertiPress(negvertibuttons[rnd.Range(0, 5)]);
+                    break;
+                case 1:
+                    posvertiPress(posvertibuttons[rnd.Range(0, 5)]);
+                    break;
+                case 2:
+                    neghoriPress(neghoributtons[rnd.Range(0, 5)]);
+                    break;
+                default:
+                    poshoriPress(poshoributtons[rnd.Range(0, 5)]);
+                    break;
+            }
         }
-      }
-      tileState();
-		}
+        tileState();
+    }
 
     void tileState()
     {
-      for(int i = 0; i <= 24; i++)
-      {
-        var letter = solveState[i];
-        var m = Array.IndexOf(currentState, letter);
-        tiles[m].material = tileColors[i];
-        tileLetters[m].text = solveState[i];
-      }
-      if (currentState.SequenceEqual(solveState))
-      {
-        GetComponent<KMBombModule>().HandlePass();
-        Debug.LogFormat("[Loopover #{0}] Module solved.", moduleId);
-        moduleSolved = true;
-      }
+        for (int i = 0; i <= 24; i++)
+        {
+            var letter = solveState[i];
+            var m = Array.IndexOf(currentState, letter);
+            tiles[m].material = tileColors[i];
+            tileLetters[m].text = solveState[i];
+        }
+        if (currentState.SequenceEqual(solveState))
+        {
+            GetComponent<KMBombModule>().HandlePass();
+            Debug.LogFormat("[Loopover #{0}] Module solved.", moduleId);
+            moduleSolved = true;
+        }
     }
 
     void negvertiPress(KMSelectable arrow)
     {
-      var ix = Array.IndexOf(negvertibuttons, arrow);
-      var s1 = currentState[0 + ix];
-      var s2 = currentState[5 + ix];
-      var s3 = currentState[10 + ix];
-      var s4 = currentState[15 + ix];
-      var s5 = currentState[20 + ix];
-      currentState[0 + ix] = s2;
-      currentState[5 + ix] = s3;
-      currentState[10 + ix] = s4;
-      currentState[15 + ix] = s5;
-      currentState[20 + ix] = s1;
+        var ix = Array.IndexOf(negvertibuttons, arrow);
+        var s1 = currentState[0 + ix];
+        var s2 = currentState[5 + ix];
+        var s3 = currentState[10 + ix];
+        var s4 = currentState[15 + ix];
+        var s5 = currentState[20 + ix];
+        currentState[0 + ix] = s2;
+        currentState[5 + ix] = s3;
+        currentState[10 + ix] = s4;
+        currentState[15 + ix] = s5;
+        currentState[20 + ix] = s1;
     }
 
     void posvertiPress(KMSelectable arrow)
     {
-      var ix = Array.IndexOf(posvertibuttons, arrow);
-      var s1 = currentState[0 + ix];
-      var s2 = currentState[5 + ix];
-      var s3 = currentState[10 + ix];
-      var s4 = currentState[15 + ix];
-      var s5 = currentState[20 + ix];
-      currentState[0 + ix] = s5;
-      currentState[5 + ix] = s1;
-      currentState[10 + ix] = s2;
-      currentState[15 + ix] = s3;
-      currentState[20 + ix] = s4;
+        var ix = Array.IndexOf(posvertibuttons, arrow);
+        var s1 = currentState[0 + ix];
+        var s2 = currentState[5 + ix];
+        var s3 = currentState[10 + ix];
+        var s4 = currentState[15 + ix];
+        var s5 = currentState[20 + ix];
+        currentState[0 + ix] = s5;
+        currentState[5 + ix] = s1;
+        currentState[10 + ix] = s2;
+        currentState[15 + ix] = s3;
+        currentState[20 + ix] = s4;
     }
 
     void neghoriPress(KMSelectable arrow)
     {
-      var ix = Array.IndexOf(neghoributtons, arrow);
-      var s1 = currentState[0 + ix * 5];
-      var s2 = currentState[1 + ix * 5];
-      var s3 = currentState[2 + ix * 5];
-      var s4 = currentState[3 + ix * 5];
-      var s5 = currentState[4 + ix * 5];
-      currentState[0 + ix * 5] = s2;
-      currentState[1 + ix * 5] = s3;
-      currentState[2 + ix * 5] = s4;
-      currentState[3 + ix * 5] = s5;
-      currentState[4 + ix * 5] = s1;
+        var ix = Array.IndexOf(neghoributtons, arrow);
+        var s1 = currentState[0 + ix * 5];
+        var s2 = currentState[1 + ix * 5];
+        var s3 = currentState[2 + ix * 5];
+        var s4 = currentState[3 + ix * 5];
+        var s5 = currentState[4 + ix * 5];
+        currentState[0 + ix * 5] = s2;
+        currentState[1 + ix * 5] = s3;
+        currentState[2 + ix * 5] = s4;
+        currentState[3 + ix * 5] = s5;
+        currentState[4 + ix * 5] = s1;
     }
 
     void poshoriPress(KMSelectable arrow)
     {
-      var ix = Array.IndexOf(poshoributtons, arrow);
-      var s1 = currentState[0 + ix * 5];
-      var s2 = currentState[1 + ix * 5];
-      var s3 = currentState[2 + ix * 5];
-      var s4 = currentState[3 + ix * 5];
-      var s5 = currentState[4 + ix * 5];
-      currentState[0 + ix * 5] = s5;
-      currentState[1 + ix * 5] = s1;
-      currentState[2 + ix * 5] = s2;
-      currentState[3 + ix * 5] = s3;
-      currentState[4 + ix * 5] = s4;
+        var ix = Array.IndexOf(poshoributtons, arrow);
+        var s1 = currentState[0 + ix * 5];
+        var s2 = currentState[1 + ix * 5];
+        var s3 = currentState[2 + ix * 5];
+        var s4 = currentState[3 + ix * 5];
+        var s5 = currentState[4 + ix * 5];
+        currentState[0 + ix * 5] = s5;
+        currentState[1 + ix * 5] = s1;
+        currentState[2 + ix * 5] = s2;
+        currentState[3 + ix * 5] = s3;
+        currentState[4 + ix * 5] = s4;
     }
 
     /*[UnityEditor.MenuItem("DoStuff/DoStuff")]
